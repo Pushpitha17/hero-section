e86810e2a315d7c83447e4cff7b7c3283a760818
Pushpitha <pushpithagee@gmail.com>
2024-02-10 14:20:12 +0530

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeroSection from './Components/HeroSection'
 
 function App() {
 
   return (
     <>
-      <div className='flex bg-black h-screen'>
-        <div className='text-white z-10 mt-80 ml-20'>
-          <p className='text-4xl font-bold'>Leaders & Champions</p>
-          <p className='text-4xl font-bold'>Are Made... Not Born!</p>
-          <div className='mt-10'>
-            <p className=''> Get Started today with our EXCLUSIVE offer!</p>
-            <p>Request more infromation today to find out how you or your child can start training!</p>
-          </div>
-        </div>
-        <div className='h-dvh w-screen'>
-          <div className='h-full flex justify-end'>
-            <div className='relative'>
-              <img src='/bg.jpg' className='h-full clip-svg' style={{ maxWidth: 'none' }} />
-              <img src="/circle.svg" className='absolute' alt="" style={{ top: '-9%', height: '118%', right: '-9%', maxWidth: 'none' }} />
-              {/* <svg className='text-right absolute'>
-                <defs>
-                  <clipPath id="myClip">
-                    <path d="M317 836.5C-262.5 474.5 84.1667 139.333 346 0.5H904.5L922 836.5" />
-                  </clipPath>
-                </defs>
-              </svg> */}
-            </div>
-          </div>
-        </div>
-
+      <div className=' bg-black'>
+        <HeroSection imgPath={'/bg.jpg'} titleText="Leaders & Champions Are Made... Not Born!" subText="Get Started today with our EXCLUSIVE offer! Request more infromation today to find out how you or your child can start training!"/>
       </div>
 
     </>
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function HeroSection({ imgPath, titleText, subText }) {
+  return (
+    <div className='max-w-screen-2xl	 ml-auto'>
+      <div className='flex h-dvh'>
+        <div className='text-white z-10  w-1/2 h-full' style={{ margin: "24% 0 0 8%" }}>
+          <div className='relative max-w-96'>
+            <p className='text-3xl font-bold'>{titleText}</p>
+            <div className='mt-10 text-sm max-w-80'>
+              <p className=''>{subText}</p>
+            </div>
+          </div>
+
+        </div>
+        <div className='w-1/2'>
+          <div className='h-full flex justify-end'>
+            <div className='relative'>
+              <img src={imgPath} className='h-full clip-svg' style={{ maxWidth: 'none' }} />
+              <img src="/circle.svg" className='absolute' alt="" style={{ top: '-9%', height: '118%', right: '-15%', maxWidth: 'none' }} />
+              {/* <svg className='text-right absolute'>
+                <defs>
+                  <clipPath id="myClip">
+                    <path d="M317 836.5C-262.5 474.5 84.1667 139.333 346 0.5H904.5L922 836.5" />
+                  </clipPath>
+                </defs>
+              </svg> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
